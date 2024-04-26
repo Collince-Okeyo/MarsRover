5377d6a6bb0a1ed294b8b9477442b49e40dd4ecd
Collince-Okeyo <collinceokeyo98@gmail.com>
2024-04-26 10:42:58 +0000

    added light and dark theme colors

--- app/src/main/java/com/dev/ramgdev/roversmars/ui/theme/Color.kt
+++ app/src/main/java/com/dev/ramgdev/roversmars/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.dev.ramgdev.roversmars.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Orange80 = Color(0xFF5C0551)
+val DarkOrange = Color(0xFF500000)
+val Orange40 = Color(0xFF0B3D91)
+val Orange10 = Color(0xFFFFCCBC)
+val LightYellow = Color(0xFFFFCC80)
+val LightBlue = Color(0xFF03A9F4)
